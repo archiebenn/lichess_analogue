84a9526da2a2d24e34645dae6b6934510727d2d6
archiebenn <archiesnbenn@gmail.com>
2026-03-01 18:07:39 +0000

    use api to retrieve game ID and print to cli

--- pi/lichess_api.py
+++ pi/lichess_api.py
@@ -10,3 +10,19 @@ client = berserk.Client(session=session)
 
 # print your profile to confirm it works
 print(client.account.get())
+
+# set up start game function that streams game state for a given game id 
+# print to console (for now)
+def start_game(game_id):
+    for event in client.board.stream_game_state(game_id):
+        
+        # UPDATE THIS FOR LEDs
+        print(event)
+
+
+# setup loop to accept incoming events using the api
+# starting a game on lichess triggers a gameStart event, and this is used to start the game loop
+for event in client.board.stream_incoming_events():
+    if event['type'] == 'gameStart':
+        game_id = event['game']['id']
+        start_game(game_id)
