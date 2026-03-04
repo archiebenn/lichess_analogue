effbef3037bf706726b9c04418816609378d7882
archiebenn <archiesnbenn@gmail.com>
2026-03-04 13:48:33 +0000

    actually fixed timers

--- pi/game.py
+++ pi/game.py
@@ -38,6 +38,29 @@ def to_from(move_string):
 
 
 ###
+# board state function - returns the current board state based on the move list
+###
+def set_board_state(moves):
+    """
+    Rebuild board state from full move list and returns None if illegal move
+    """
+    # rebuild board from full move list to ensure correct state
+    board = chess.Board()
+
+    try:
+        # move is valid, push to board state
+        for uci in moves.split():
+            board.push_uci(uci)
+        return board
+
+    # illegal move. don't crash on malformed/unknown moves and skip until next update
+    except chess.IllegalMoveError as e:
+        print(f"Warning: illegal move in moves list, skipping: {e}")
+        return None
+
+
+
+###
 # whose_turn function - returns which player's turn it is based on game state
 ###
 
@@ -65,24 +88,50 @@ def is_my_turn(moves, my_colour):
         return not whites_turn_next
     
 
+def handle_turn(moves, my_colour, origin, destination, white_time, black_time):
+    """
+    Handles whose turn it is, returns time remaining for current player for timer function, and will eventually handle LED updates
+    """
+    # my turn - light LEDs and set timer counting down
+    if is_my_turn(moves, my_colour):
+
+        # light up LEDs to show previous (opponent's) move
+        LED_instruction(origin, destination)
+
+        # set variables to pass to timer() for my turn time remaining
+        time_ms = white_time if my_colour == 'white' else black_time
+        player_label = "Your time remaining"
+
+                
+    else:
+        # show previous move
+        print(f"(Your move) {origin} -> {destination}")
+
+        # set variables to pass to timer() for opponent's time remaining
+        time_ms = white_time if my_colour != 'white' else black_time
+        player_label = "Opponent time remaining"
+
+    return time_ms, player_label
+    
+
 
 ###
-# timer function - returns players' remaining times at 0.1s intervals
+# timer function - returns players' remaining times at 1s intervals
 ###
-def timer(time_ms, player_label):
+def timer(time_ms, player_label, stop_event):
     """
-    converts ms to seconds and formats for display
+    Converts ms to seconds and formats for display
     """
-    _stop_clock.clear()
+    stop_event.clear()
 
-    # convert timedelta to seconds if needed
+    # convert timedelta to secondds
     if hasattr(time_ms, 'total_seconds'):
         remaining_secs = time_ms.total_seconds()
     else:
         remaining_secs = time_ms / 1000
 
     # calculate only when time on clock and set as integers
-    while not _stop_clock.is_set() and remaining_secs > 0:
+    while not stop_event.is_set() and remaining_secs > 0:
         minutes = int(remaining_secs)//60
         seconds = int(remaining_secs) % 60
 
@@ -90,9 +139,40 @@ def timer(time_ms, player_label):
         print(f"\r{player_label}: {minutes:02d}:{seconds:02d}", end="", flush=True)
 
         # print every 0.1s interval
-        time.sleep(0.1)
-        remaining_secs -= 0.1
+        time.sleep(1)
+        remaining_secs -= 1
     print()
+
+
+
+###
+# start stop timer function - controls starting and stopping of timer thread based on game state and turn
+### 
+def start_stop_timer(clock_thread, stop_clock, white_time, black_time, time_ms, player_label):
+
+    # only start timer on timed games where times != None
+    if white_time and black_time:
+
+        # stop any  previous countdown if running
+        if clock_thread and clock_thread.is_alive():
+            stop_clock.set()
+            clock_thread.join()
+
+        # create new stop event for this timer
+        stop_clock = threading.Event()
+
+        # run timer() function in separate thread to avoid blocking main game loop
+        clock_thread = threading.Thread(target=timer, args=(time_ms, player_label, stop_clock), daemon=True)
+        clock_thread.start()
+                
+    else:
+
+        # unlimited time games, stop any running timer if it exists
+        if clock_thread and clock_thread.is_alive():
+            stop_clock.set()
+            clock_thread.join()
+
+    return clock_thread, stop_clock
 
 
 
@@ -103,14 +183,14 @@ def timer(time_ms, player_label):
 # any move made in the game will trigger an update to the game state, which is streamed in real time and can be used to update the board/cli/LEDs (eventually)
 def start_game(client, game_id, my_colour):
     """
-    streams game state for a given game id and updates LEDs/CLI accordingly
+    Streams game state for a given game id and updates LEDs/CLI accordingly
     """
 
     board = chess.Board()
     
     # timers 
     _clock_thread = None
-    _stop_clock = threading.Event()
+    _stop_clock = None
 
     # set statuses indicating if a game has already finished to avoid attempting to process moves during a finished game
     finished_status = {'resign', 'mate', 'timeout', 'draw', 'stalemate', 'aborted', 'outoftime'}
@@ -123,14 +203,19 @@ def start_game(client, game_id, my_colour):
 
             # check if game has finished first before attemtping to process moves
             if event['status'] in finished_status:
-                print(f"Game {game_id} has finished: {event['status']}")
+
+                # stop any running timer before returning to main.py (stops timer running into next game)
+                if _clock_thread and _clock_thread.is_alive():
+                    _stop_clock.set()
+                    _clock_thread.join()
 
                 # return to main.py to wait for next game
                 return
             
+            # PROCESS MOVE STUFF
             # print only latest move from streamed game state:
             moves = event['moves']
-            # PROCESS MOVE STUFF
+
             if moves:
 
                 # latest move string
@@ -140,17 +225,10 @@ def start_game(client, game_id, my_colour):
                 origin, destination, promotion = to_from(latest_move)
 
                 # rebuild board from full move list to ensure correct state
-                board = chess.Board()
+                board = set_board_state(moves)
 
-                try:
-                    for uci in moves.split():
-                        board.push_uci(uci)
-
-                # don't crash on malformed/unknown moves and skip until next update
-                # if there is an error which is corrected it will print a warning but then update chess.Board() on next move anyway
-                except chess.IllegalMoveError as e:
-
-                    print(f"Warning: illegal move in moves list, skipping: {e}")
+                # skip on error
+                if board is None:
                     continue
 
                 # print CLI chess board
@@ -164,48 +242,12 @@ def start_game(client, game_id, my_colour):
                 white_time = event.get('wtime')
                 black_time = event.get('btime')
 
-
-                # LIGHT STUFF
-                # my turn - light LEDs and set timer counting down
-                if is_my_turn(moves, my_colour):
-
-                    # light up LEDs to show previous (opponent's) move
-                    LED_instruction(origin, destination)
-
-                    # set variables to pass to timer() for my turn time remaining
-                    time_ms = white_time if my_colour == 'white' else black_time
-                    player_label = "Your time remaining"
-
-                
-                else:
-                    # show previous move
-                    print(f"(Your move) {origin} -> {destination}")
-
-                    # set variables to pass to timer() for opponent's time remaining
-                    time_ms = white_time if my_colour != 'white' else black_time
-                    player_label = "Opponent time remaining"
-
+                # HANDLE TURN, TIMERS AND LEDS
+                time_ms, player_label = handle_turn(moves, my_colour, origin, destination, white_time, black_time)                
 
                 # TIMER STUFF
-                # only start timer on timed games where times != None
-                if white_time and black_time:
+                _clock_thread, _stop_clock = start_stop_timer(_clock_thread, _stop_clock, white_time, black_time, time_ms, player_label)
 
-                    # stop any  previous countdown if running
-                    if _clock_thread and _clock_thread.is_alive():
-                        _stop_clock.set()
-                        _clock_thread.join()
-
-                    # run timer() function in separate thread to avoid blocking main game loop
-                    _clock_thread = threading.Thread(target=timer, args=(time_ms, player_label), daemon=True)
-                    _clock_thread.start()
-                
-                else:
-
-                    # unlimited time games, stop any running timer if it exists
-                    if _clock_thread and _clock_thread.is_alive():
-                        _stop_clock.set()
-                        _clock_thread.join()
-                        
 
             
                     
